2b4f2088834a05c536c1e9a5e167cc40a3d06937
unknown <partharora557@gmail.com>
2023-03-03 15:04:28 +0530

    urgent-changes

--- src/components/leads/Container/index.tsx
+++ src/components/leads/Container/index.tsx
@@ -3,7 +3,8 @@ import React from "react";
 const LeadsContainer = ({ children }: LeadContainerProps) => {
   return (
     <div className="w-[100%] bg-white min-h-[70vh] rounded-[24px]">
-      <div className="w-[100%] h-[50px] border-b-[1px] border-[#ccc]">{children}</div>
+      <div className="w-[100%] h-[50px] border-b-[1px] border-[#ccc]"></div>
+      {children}
     </div>
   );
 };
