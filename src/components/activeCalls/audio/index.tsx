f67d61b318ea8a931f065af113992402d88ba23d
unknown <partharora557@gmail.com>
2023-03-10 14:27:16 +0530

    transscript added

--- src/components/activeCalls/audio/index.tsx
+++ src/components/activeCalls/audio/index.tsx
@@ -1,10 +1,81 @@
 import React from "react";
+import Tracker from "../conversation-tracker";
+import Transcript from "../Transcript/Transcript";
 
-const Audio=()=>{
-    return(
-        <>
-        </>
-    )
-}
+const list = [
+  {
+    duration: 40,
+    at: 20,
+  },
+  {
+    duration: 60,
+    at: 80,
+  },
+  {
+    duration: 30,
+    at: 160,
+  },
+  {
+    duration: 40,
+    at: 210,
+  },
+  {
+    duration: 40,
+    at: 280,
+  },
+  {
+    duration: 40,
+    at: 320,
+  },
+  {
+    duration: 40,
+    at: 400,
+  },
+  {
+    duration: 40,
+    at: 500,
+  },
+];
+
+const Audio = () => {
+  return (
+    <>
+      {/* <div className="w-full relative p-[20px] ">
+        <div className="w-full h-full flex  absolute z-10 top-0 left-0 items-center justify-center ">
+          <div className="w-[100px] h-[100px] cursor-pointer flex items-center justify-center rounded-[34px] bg-white">
+            <img src="/Play.svg" className="" alt="" />
+          </div>
+        </div>
+        <img src="/video.svg" className="w-full rounded-2xl" alt="" />
+      </div> */}
+      <div className="w-full mt-[30px] mb-[30px] px-[38px] ">
+        <h1 className="text-[16px] font-medium text-black uppercase mb-[10px]">
+          call data
+        </h1>
+        <div className="w-full flex items-center">
+          <img
+            src="/Images/dots/Dot.svg"
+            className="h-[18px] svg-ellipse-5 mr-[25px]"
+            alt=""
+          />
+          <p className="text-[#304FFD] text-[14px]">Participant 1:John</p>
+        </div>
+        <div className="w-full flex items-center mt-[6px]">
+          <img
+            src="/Images/dots/Dot.svg"
+            className="h-[18px] svg-ellipse-6 mr-[25px]"
+            alt=""
+          />
+          <p className="text-[#FF965D] text-[14px]">Participant 2:Shraddha</p>
+        </div>
+      </div>
+      <Tracker title={"John"} list={list} color={"#304FFD"} />
+      <Tracker title={"Shraddha"} list={list} color={"#FF965D"} />
+      <Tracker title={"Topics"} list={list} color={"#0090FF"}/>
+      
+      <Transcript />
+    </>
+  );
+};
 
 export default Audio;
---
+++ src/components/activeCalls/conversation-tracker/index.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+const Tracker = ({ color, title, list }: any) => {
+  return (
+    <div className="w-full px-[38px] my-[24px]">
+      <h1
+        className={` text-[14px] font-medium`}
+        style={{
+          color: color,
+        }}
+      >
+        {title}
+      </h1>
+      <div className="w-[100%] bg-[#efefef] h-[11px] mt-[5px] relative ">
+        {list.map((item: any) => {
+          return (
+            <div
+              className="h-[11px] absolute"
+              style={{
+                width: item.duration,
+                backgroundColor: color,
+                top: 0,
+                left: item.at,
+              }}
+            ></div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Tracker;
