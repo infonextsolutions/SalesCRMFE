cd17de4db4537c218be4e4fd5b6b22f94e7a15ee
Apurv Jain <jainapurv35@gmail.com>
2023-03-13 23:27:51 +0530

    pagination

--- src/components/View/Tables/Leads.tsx
+++ src/components/View/Tables/Leads.tsx
@@ -1,20 +1,75 @@
 import Lead from "@/types/Leads";
 import ButtonDropDown from "@/utils/Button/Button";
 import SmallButton from "@/utils/Button/SmallButton";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import LeadContainer from "../../leads/Lead/Lead";
 import Header from "../../leads/Header/Header";
-
-const LeadsTable = ({ result, totalRecords }: TableProps) => {
+import ReactPaginate from "react-paginate";
+import Image from "next/image";
+import { getBasicIcon, LeftArrow, LeftDoubleArrow, RightArrow } from "@/utils/AssetsHelper";
+const LeadsTable = ({ totalRecords:any }: TableProps) => {
   // console.log(AllLeads);
-  const totalLeads = totalRecords;
-  const Leads = result;
+  const [pageCount,setpageCount]:any = useState([]);
+  const [pageNumber,setpageNumber] :any = useState(0);
+  const [limit,setLimit] :any = useState(10);
+  // setpageCount(totalRecords);
+  const [items,setItems]:any = useState([]);
   
+  useEffect(()=>{
+
+    const getItems = async () =>{
+      const res = await fetch(
+        `https://testsalescrm.nextsolutions.in/api/leads/find-all?limit=${limit}&page=${pageNumber}`
+      );
+      const data = await res.json();
+      // console.log(data);
+      
+      setItems(data);
+      setpageCount(Math.ceil(items.totalRecords/limit));
+    }
+
+    getItems();
+  },[limit])
+
+  // console.log(items.result);
+  const fetchItems = async (current:any) =>{
+    const res = await fetch(
+      `https://testsalescrm.nextsolutions.in/api/leads/find-all?limit=${limit}&page=${current}`
+    );
+    const data = await res.json();
+    return data;
+  }
+
+  const handleChange = (e:any) =>{
+    setLimit(e.target.value);
+  }
+
+  const setFirstPage = async () =>{
+    setpageNumber(0);
+    const allItems = await fetchItems(pageNumber);
+    setItems(allItems);
+  }
+  const setLastPage = async () =>{
+    setpageNumber(pageCount-1);
+    const allItems = await fetchItems(pageNumber);
+    setItems(allItems);
+  }
+  const handlePageClick = async (data:any) =>{
+    // console.log(data.selected);
+
+    let current = data.selected;
+    setpageNumber(current);
+    const allItems = await fetchItems(current);
+    setItems(allItems);
+
+  }
+  const Leads = items.result;
+  // console.log(Leads);
   return (
     <>
       <div className="mt-[0px] w-[100%] h-[540px]  overflow-x-auto  hide-scrollbar">
         <Header />
-        {Leads.map((item: Lead) => {
+        {Leads!=null&&Leads.map((item: Lead) => {
           return (
             <LeadContainer
               key={item._id}
@@ -29,7 +84,102 @@ const LeadsTable = ({ result, totalRecords }: TableProps) => {
           );
         })}
       </div>
-      <div className="flex h-[80px] items-center justify-between ">
+      <div className="mx-[80px] flex justify-between">
+        <div className="flex items-center">
+          <select onChange={handleChange} name="limit" id="limit" className="bg-[#fff] border border-[#8A9099] rounded-[10px] text-black p-[5px] px-[8px]">
+          <option value="10" selected>10</option>
+          <option value="11">11</option>
+          <option value="12">12</option>
+          <option value="13">13</option>
+          </select>
+          <p className="ml-[12px] text-norm text-[14px] font-medium tracking-wider">
+            {`Showing 1-${limit} of ${items.totalRecords}`}
+          </p>
+        </div>
+        <div className="flex justify-center my-[45px] ">
+          <div 
+          className={`flex justify-center mr-[8px] h-[40px] w-[40px] cursor-pointer rounded-[10px] ${pageNumber===0?'bg-[#f5f5f5]':'bg-[#e8ebfd]'}`}
+          onClick={setFirstPage}
+          >
+            <Image
+              src={getBasicIcon("Arrow-Right 2")}
+              className={`${pageNumber!=0?"svg-blue":''} rotate-180 translate-x-[6px]`}
+              alt=""
+              width={18}
+              height={18}
+            />
+            <Image
+              src={getBasicIcon("Arrow-Right 2")}
+              className={`${pageNumber!=0?"svg-blue":''} rotate-180 translate-x-[-6px]`}
+              alt=""
+              width={18}
+              height={18}
+            />
+          </div>
+          <ReactPaginate
+          previousLabel={
+            <Image
+              src={getBasicIcon("Arrow-Right 2")}
+              className={`${pageNumber!=0?"svg-blue":''} rotate-180`}
+              alt=""
+              width={20}
+              height={20}
+              style={{
+                objectFit: "contain",
+              }}
+            />}
+          nextLabel={
+            <Image
+              src={getBasicIcon("Arrow-Right 2")}
+              className={`${pageNumber!=pageCount-1?"svg-blue":''}`}
+              alt=""
+              width={16}
+              height={16}
+              style={{
+                objectFit: "contain",
+              }}
+            />}
+          breakLabel={'...'}
+          pageCount={pageCount}
+          marginPagesDisplayed={2}
+          pageRangeDisplayed={0}
+          onPageChange={handlePageClick}   
+          containerClassName={'text-black flex justify-center gap-[8px]'}  
+          pageClassName={'px-[15px] py-[8px] text-[15px] text-[#3F434A]'}
+          pageLinkClassName={``}
+          previousClassName={`flex justify-center  px-[10px] py-[7px] rounded-[10px] ${pageNumber===0?'bg-[#f5f5f5]':'bg-[#e8ebfd]'}`}
+          previousLinkClassName={`flex justify-center ${pageNumber!=0?'text-[#304FFD]':'cursor-auto'}`}
+          nextClassName={`flex justify-center  px-[10px] py-[7px] rounded-[10px] ${pageNumber===pageCount-1?'bg-[#f5f5f5]':'bg-[#e8ebfd]'}`}
+          nextLinkClassName={`flex justify-center ${pageNumber===pageCount-1?'cursor-auto':''}`}
+          breakClassName={''}
+          breakLinkClassName={''}
+          activeClassName={'bg-renal-blue text-[#fff] rounded-[10px]'}
+          />
+          <div 
+          className={`flex justify-center ml-[8px] h-[40px] w-[40px] cursor-pointer rounded-[10px] ${pageNumber===0?'bg-[#f5f5f5]':'bg-[#e8ebfd]'}`}
+          onClick={setLastPage}
+          >
+            <Image
+              src={getBasicIcon("Arrow-Right 2")}
+              className={`${pageNumber!=0?"svg-blue":''} translate-x-[6px]`}
+              alt=""
+              width={18}
+              height={18}
+            />
+            <Image
+              src={getBasicIcon("Arrow-Right 2")}
+              className={`${pageNumber!=0?"svg-blue":''} translate-x-[-6px]`}
+              alt=""
+              width={18}
+              height={18}
+            />
+          </div>
+        </div>
+      </div>
+      
+      
+      
+      {/* <div className="flex h-[80px] items-center justify-between ">
         <div className="flex items-center">
           <ButtonDropDown
             width={80}
@@ -70,7 +220,7 @@ const LeadsTable = ({ result, totalRecords }: TableProps) => {
           <SmallButton RightDblIcon={true} theme={4} left={10} />
           <SmallButton RightIcon={true} theme={4} left={10} />
         </div>
-      </div>
+      </div> */}
       <div className="h-[10px] w-full"></div>
     </>
   );
@@ -79,6 +229,7 @@ const LeadsTable = ({ result, totalRecords }: TableProps) => {
 export default LeadsTable;
 
 interface TableProps {
-  result: Lead[] | any;
   totalRecords: Number;
+  [key: string]: any;
+  icon?: String;
 }
