ef19d1f764eb5ad9122c3c2ab423f26688690cfe
Apurv Jain <jainapurv35@gmail.com>
2023-03-14 16:35:21 +0530

    Add lead

---
+++ src/pages/sales/open/[id]/add-lead.tsx
@@ -0,0 +1,40 @@
+import Navigation from "@/components/app/Navigation";
+import LeadsContainer from "@/components/leads/Container";
+import Table from "@/components/View/Tables/Leads";
+import axios from "axios";
+import React from "react";
+import KanbanContainer from "@/components/View/Kanban";
+import { Router, useRouter } from "next/router";
+
+
+export default function Open({data}:any) {
+  return (
+    <div className="w-[100%] min-h-[90vh] pl-[40px] pr-[40px]">
+        <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+            <div className="mb-4">
+            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="contactName">
+                Contact Name
+            </label>
+            <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="contactName" type="text" placeholder="Contact Name"/>
+            </div>
+            <div className="mb-4">
+            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="contactNumber">
+                Contact Number
+            </label>
+            <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="contactNumber" type="text" placeholder="Contact Number"/>
+            </div>
+            <div className="mb-4">
+            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
+                Email
+            </label>
+            <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email" type="email" placeholder="Email"/>
+            </div>
+            <div className="flex items-center justify-between">
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
+                Submit
+            </button>
+            </div>
+        </form>
+    </div>
+  );
+}
