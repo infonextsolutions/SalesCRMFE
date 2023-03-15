eb5a418c948656684aa9aa01022d56f787f6fbf7
unknown <partharora557@gmail.com>
2023-03-15 10:02:22 +0530

    subit

--- src/components/calls/Container/Container.tsx
+++ src/components/calls/Container/Container.tsx
@@ -1,15 +1,21 @@
+import Kanban from "@/components/View/Kanban";
+import CallsTable from "@/components/View/Tables/Calls";
 import ButtonDropDown from "@/utils/Button/Button";
 import React from "react";
 import Search from "../Search/Search";
 
-const CallsContainer = ({ children }: LeadContainerProps) => {
+const CallsContainer = ({ view,dummy1,dummy2}: LeadContainerProps) => {
   return (
     <div className="w-[100%] bg-white min-h-[70vh] rounded-[18px] overflow-hidden mb-[40px]">
       <div className="w-[100%] h-[58px] flex items-center  px-[8px]">
         <Search />
         <ButtonDropDown light={true} text={"Actions"} border={true} id={1} dropdown={true} list={[]} />
       </div>
-      {children}
+      {!view ? (
+          <CallsTable result={dummy1.result} totalRecords={dummy1.totalRecords} />
+        ) : (
+          <Kanban list={dummy2} />
+        )}
     </div>
   );
 };
@@ -17,5 +23,7 @@ const CallsContainer = ({ children }: LeadContainerProps) => {
 export default CallsContainer;
 
 interface LeadContainerProps {
-  children: JSX.Element[] | JSX.Element;
+  view:Boolean;
+  dummy1:any;
+  dummy2:any;
 }
