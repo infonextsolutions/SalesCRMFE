bd4b61cbc825c892f7476a9587fcb289ec1e546b
unknown <partharora557@gmail.com>
2023-03-15 11:51:55 +0530

    done

--- src/pages/sales/open/add-lead.tsx
+++ src/pages/sales/open/add-lead.tsx
@@ -6,35 +6,109 @@ import React from "react";
 import KanbanContainer from "@/components/View/Kanban";
 import { Router, useRouter } from "next/router";
 
-
-export default function Open({data}:any) {
+export default function Open({ data }: any) {
   return (
     <div className="w-[100%] min-h-[90vh] pl-[40px] pr-[40px]">
+      {/* <Navigation  /> */}
+      <Navigation
+        title={"Sales-Manage>Leads"}
+        buttons={[
+          {
+            text: "View",
+            dropdown: true,
+            id: 0,
+            light: false,
+            list: [
+              {
+                title: "Table View",
+                Icon: "List 2",
+              },
+              {
+                title: "Kanban View",
+                Icon: "Grid",
+              },
+            ],
+          },
+          {
+            text: "Add Lead",
+            dropdown: true,
+            id: 1,
+            icon: "Plus",
+            light: false,
+            list: [
+              { title: "Using Form", Icon: "Text" },
+              { title: "Import Leads", Icon: "Download" },
+            ],
+          },
+          {
+            text: "Export",
+            dropdown: true,
+            id: 1,
+            icon: "Download",
+            light: true,
+            list: [
+              { title: "Print", Icon: "Printer" },
+              { title: "Excel", Icon: "Excel" },
+              { title: "PDF", Icon: "PDF" },
+              { title: "CSV", Icon: "CSV" },
+            ],
+          },
+        ]}
+      />
+      <div className="w-[100%] min-h-[90vh] pl-[40px] pr-[40px]">
         <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-            <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="contactName">
-                Contact Name
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="contactName"
+            >
+              Contact Name
             </label>
-            <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="contactName" type="text" placeholder="Contact Name"/>
-            </div>
-            <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="contactNumber">
-                Contact Number
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="contactName"
+              type="text"
+              placeholder="Contact Name"
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="contactNumber"
+            >
+              Contact Number
             </label>
-            <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="contactNumber" type="text" placeholder="Contact Number"/>
-            </div>
-            <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
-                Email
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="contactNumber"
+              type="text"
+              placeholder="Contact Number"
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="email"
+            >
+              Email
             </label>
-            <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email" type="email" placeholder="Email"/>
-            </div>
-            <div className="flex items-center justify-between">
-            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
-                Submit
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="email"
+              type="email"
+              placeholder="Email"
+            />
+          </div>
+          <div className="flex items-center justify-between">
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              type="submit"
+            >
+              Submit
             </button>
-            </div>
+          </div>
         </form>
+      </div>
     </div>
   );
 }
