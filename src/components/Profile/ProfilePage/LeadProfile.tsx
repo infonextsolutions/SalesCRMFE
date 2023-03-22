388327a686ec66ae263d49ac8ecef0b2f69f15fa
Manya shree <mayavangimalla@gmail.com>
2023-03-22 14:22:54 +0530

    lead/companycss

--- src/components/Profile/ProfilePage/LeadProfile.tsx
+++ src/components/Profile/ProfilePage/LeadProfile.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { getBasicIcon } from "@/utils/AssetsHelper";
+import { getBasicIcon, getRoundedAvatar } from "@/utils/AssetsHelper";
 import Image from "next/image";
 
 const ProfilePage = () => {
@@ -11,16 +11,16 @@ const ProfilePage = () => {
         {/* <div className="relative inline-flex items-start mr-10"></div> */}
         {/* <div className="flex items-center justify-between"></div> */}
         <div className="flex items-center w-[100%] border-gray-300 border-b-[1px] pb-[30px] pt-[20px] ">
-          {/* <Image
+          <Image
             className="inline-block w-[60px] rounded-full ring-2 ring-white"
-            src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.25&w=256&h=256&q=80"
+            src={getRoundedAvatar(5, 30)}
             alt=""
             width={60}
             height={60}
             style={{
               objectFit: "contain",
             }}
-          /> */}
+          /> 
           <div className="ml-4">
             <h2 className="text-base text-[14px] whitespace-nowrap leading-7 tracking-tight text-gray-900 font-medium">
               LEAD-info
@@ -51,25 +51,25 @@ const ProfilePage = () => {
       <ul className="mt-2 mb-10 ml-[1px]">
         <li className="px-2 mt-4">
           <strong className="font-medium mr-1 text-gray-500">Company Name</strong>
-          <a href="name" className="block text-black-400">
+          <a href="name" className="block text-black">
             ABC Corp
           </a>
         </li>
         <li className="px-2 mt-4">
           <strong className="font-medium mr-1 text-gray-500">Company Address</strong>
-          <a href="address:" className="block text-black-400">
+          <a href="address:" className="block text-black">
             nothing
           </a>
         </li>
         <li className="px-2 mt-4">
           <strong className="font-medium mr-1 text-gray-500">Website Link</strong>
-          <span className="block text-black-400">
+          <span className="block text-black">
             <a href="https://www.example.com">www.example.com</a>
           </span>
         </li>
         <li className="px-2 mt-4">
           <strong className="font-medium mr-1 text-gray-500">Industry Type</strong>
-          <a href="industry:" className="block text-black-400">
+          <a href="industry:" className="block text-black">
             IT Solutions
           </a>
         </li>
@@ -104,43 +104,43 @@ const ProfilePage = () => {
       <h3 className="text-sm font-medium mt-5 ml-3 text-black">LEAD INFO</h3>
       <ul className="mt-2 mb-10 ml-1">
         <li className="px-2 mt-4">
-          <strong className="font-medium mr-1 text-black-400">Lead Stage</strong>
-          <a href="name" className="block text-black-400">
+          <strong className="font-medium mr-1 text-black">Lead Stage</strong>
+          <a href="name" className="block text-gray">
             XYZ
           </a>
         </li>
         <li className="px-2 mt-4">
-          <strong className="font-medium mr-1 text-black-400">Lead Source</strong>
-          <a href="address:" className="block text-black-400">
+          <strong className="font-medium mr-1 text-black">Lead Source</strong>
+          <a href="address:" className="block text-gray">
             nothing
           </a>
         </li>
         <li className="px-2 mt-4">
-          <strong className="font-medium mr-1 text-black-400">Lead Owner(primary)</strong>
-          <span className="block text-black-400">
+          <strong className="font-medium mr-1 text-black">Lead Owner(primary)</strong>
+          <span className="block text-gray">
             <a href="Leadowner">Aarti.S</a>
           </span>
         </li>
         <li className="px-2 mt-4">
-          <strong className="font-medium mr-1 text-black-400">Lead Owner(secondary)</strong>
-          <a href="leadowner" className="block text-black-400">
+          <strong className="font-medium mr-1 text-black">Lead Owner(secondary)</strong>
+          <a href="leadowner" className="block text-gray">
             Raghav V.
           </a>
         </li>
         <li className="px-2 mt-4">
-          <strong className="font-medium mr-1 text-black-400">Lead Created By</strong>
-          <a href="leadowner" className="block text-black-400">
+          <strong className="font-medium mr-1 text-black">Lead Created By</strong>
+          <a href="leadowner" className="block text-gray">
             -
           </a>
         </li>
         <li className="px-2 mt-4">
-          <strong className="font-medium mr-1 text-black-400">Lead Updated on</strong>
-          <a href="leadowner" className="block text-black-400">
+          <strong className="font-medium mr-1 text-black">Lead Updated on</strong>
+          <a href="leadowner" className="block text-gray">
             26 January 2023 4:55 PM
           </a>
         </li>
         <li className="px-2 mt-4">
-          <strong className="font-medium mr-1 text-black-400">Activity History</strong>
+          <strong className="font-medium mr-1 text-black">Activity History</strong>
         </li>
         <div
           className={`flex h-[20px] items-center justify-between mt-[5px] px-2 shrink-0`}
@@ -217,12 +217,17 @@ const ProfilePage = () => {
       <div className="py-1"></div>
       <h3 className="text-sm font-medium mt-5 ml-3 text-black">PRIMARY CLIENT POC</h3>
       <div className="flex items-center w-[100%] pb-[30px] pt-[20px] ">
-        {/* <Image
-          className="inline-block w-[55px] rounded-full ring-2 ring-white"
-          src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.25&w=256&h=256&q=80"
-          alt=""
-        /> */}
-        <div className="ml-3">
+      <Image
+              className="h-12 w-16 rounded-full ml-2"
+              src={getRoundedAvatar(2, 30)}
+              alt=""
+              width={64}
+              height={48}
+              style={{
+                objectFit:"contain"
+              }}
+            /> 
+        <div className="ml-2">
           <h2 className="text-base text-[14px] whitespace-nowrap leading-7 tracking-tight text-gray-900 font-medium">
             Sharddha P
           </h2>
@@ -238,19 +243,19 @@ const ProfilePage = () => {
       <ul className="mt-2 mb-10 ml-auto">
         <li className="px-2 mt-4">
           <strong className="font-medium mr-1 text-gray-500">GENDER</strong>
-          <a href="tel:+821023456789" className="block text-black-400">
+          <a href="tel:+821023456789" className="block text-black">
             Female
           </a>
         </li>
         <li className="px-2 mt-4">
           <strong className="font-medium mr-1 text-gray-500">E-mail</strong>
-          <a href="mailto:" className="block text-black-400">
+          <a href="mailto:" className="block text-black">
             aspiringfe@helloworld.com
           </a>
         </li>
         <li className="px-2 mt-4">
           <strong className="font-medium mr-1 text-gray-500">Location</strong>
-          <span className="block text-black-400">Seoul, South Korea</span>
+          <span className="block text-black">Seoul, South Korea</span>
         </li>
         <li className="px-2 mt-4">
           <strong className="font-medium mr-1 text-gray-500">Social Media</strong>
@@ -300,16 +305,16 @@ const ProfilePage = () => {
       >
         <li>
           <div className="flex items-center gap-x-3 mr-3">
-            {/* <Image
+            <Image
               className="h-12 w-16 rounded-full ml-2"
-              src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+              src={getRoundedAvatar(6, 30)}
               alt=""
               width={64}
               height={48}
               style={{
                 objectFit:"contain"
               }}
-            /> */}
+            /> 
             <div>
               <h4 className="text-base font-semibold leading-7 tracking-tight text-gray-900">
                 LeslieAlexander
@@ -331,11 +336,16 @@ const ProfilePage = () => {
       >
         <li>
           <div className="flex items-center gap-x-3 mr-3">
-            {/* <Image
+          <Image
               className="h-12 w-16 rounded-full ml-2"
-              src="https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+              src={getRoundedAvatar(4, 30)}
               alt=""
-            ></Image> */}
+              width={64}
+              height={48}
+              style={{
+                objectFit:"contain"
+              }}
+            />  
             <div>
               <h4 className="text-base font-semibold leading-7 tracking-tight text-gray-900">
                 Reginacooper
@@ -357,11 +367,16 @@ const ProfilePage = () => {
       >
         <li>
           <div className="flex items-center gap-x-3 mr-3">
-            {/* <Image
+          <Image
               className="h-12 w-16 rounded-full ml-2"
-              src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+              src={getRoundedAvatar(3, 30)}
               alt=""
-            /> */}
+              width={64}
+              height={48}
+              style={{
+                objectFit:"contain"
+              }}
+            /> 
             <div>
               <h4 className="text-base font-semibold leading-7 tracking-tight text-gray-900">
                 Judith
