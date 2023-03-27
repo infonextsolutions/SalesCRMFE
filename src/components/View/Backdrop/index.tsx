a9c330ed8a468f62a7095c2d5e266f67609e3db3
unknown <partharora557@gmail.com>
2023-03-27 08:00:02 +0530

    dome

--- src/components/View/Backdrop/index.tsx
+++ src/components/View/Backdrop/index.tsx
@@ -4,7 +4,11 @@ const Backdrop = ({ children }: any) => {
 
   return (
     <>
-      <div className="fixed top-0 left-0 w-[100%] h-[100%] bg-black opacity-[0.3]">{children}</div>
+      <div className="fixed top-0 left-0 w-[100%] h-[100%] z-100  bg-black opacity-[0.3]"
+        style={{
+          zIndex:1000
+        }}
+      >{children}</div>
     </>
   );
 };
