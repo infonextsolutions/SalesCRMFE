358105d47e93166b13414816433cfc491748abb8
unknown <partharora557@gmail.com>
2023-03-27 09:36:58 +0530

    done

--- src/components/View/import-lead/Index.tsx
+++ src/components/View/import-lead/Index.tsx
@@ -1,11 +1,7 @@
 import React from "react";
 
-const ImportLead=()=>{
+const ImportLead = () => {
+  return <></>;
+};
 
-    return(
-        <>
-        </>
-    )
-}
-
-export default ImportLead
+export default ImportLead;
