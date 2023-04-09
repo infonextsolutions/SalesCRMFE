0e17b7beec9b1d32b1f559b89327d043d7c35f1d
Manya shree <mayavangimalla@gmail.com>
2023-04-10 02:04:14 +0530

    noteschanges

--- src/components/Profile/AudioProfileDetails/Comment.tsx
+++ src/components/Profile/AudioProfileDetails/Comment.tsx
@@ -5,13 +5,14 @@ import React from "react";
 
 const Comments = () => {
   return (
+    
     <><div className="mt-[10px] ml-[20px] w-[85%] h-[120px] border-[#ccc] border-[1px] rounded-[20px] flex ">
-      <p className="mt-[10px] mb-[25px] ml-[6px] text-gray-400">write a comment here....</p>
-    </div>
+    <p className="mt-[10px] mb-[25px] ml-[6px] text-gray-400">write a comment here</p>
+  </div>
     <div className="py-2">
-    <div className="bg-renal-blue rounded-xl items-center justify-center pl-[10px] mr-[50px] ml-[320px] pr-[10px] p-[10px]">
-    <p className="whitespace-nowrap font-large text-[14px] pl-[10px] pr-[10px] text-[#ffffff] ">comment</p>
-    </div>
+    <button className="bg-renal-blue rounded-xl items-center justify-center pl-[10px] mr-[50px] ml-[320px] pr-[10px] p-[7px]">
+    <p className="whitespace-nowrap font-large text-[15px] pl-[10px] pr-[10px] text-[#ffffff] ">comment</p>
+    </button>
     <div className="mx-auto w-[100%] border-b border-gray-300 my-3"></div>
     <div className="py-1"></div>
     <div className="flex items-center justify-between mt-2 ml-4">
@@ -24,7 +25,7 @@ const Comments = () => {
     <p className="mr-20 text-blue-500">@Micheal</p>
     </div>
     <p className="ml-7 text-blue-500">#introcall</p>
-    <h3 className="py-2 text-gray-500 text-small font-medium ml-7">Reply</h3>
+    <button className="py-2 text-gray-500 text-small font-medium ml-7">Reply</button>
     <div className="flex items-center justify-between mt-2 ml-4">
     <h3 className="text-sm text-black font-medium ml-8">Micheal</h3>
     <p className="text-sm text-gray-600 mr-10">16h ago</p>
