f33578a309ebe101efd4551545d0a3377233d2c4
Manya shree <mayavangimalla@gmail.com>
2023-04-12 16:20:21 +0530

    minorchanges

--- src/components/Profile/ProfileDetails/Lead/attachements.tsx
+++ src/components/Profile/ProfileDetails/Lead/attachements.tsx
@@ -7,35 +7,45 @@ const Attachements = () => {
     <>
       <div className="bg-[#ffffff] my-[30px]">
         <div className="flex">
-      <h2 className="text-[#3F434A] text-2xl font-medium">Attachments</h2>
-      <button className="ml-[380px] flex bg-renal-blue pl-[6px] rounded-xl pr-[6px] p-[8px]">
-      <Image
-        src={getBasicIcon("upload")}
-        className={`w-[18px] svg-white`}
-        alt=""
-        width={20}
-        height={20}
-        style={{
-          objectFit: "contain",
-        }}
-      />
-      <p className="whitespace-nowrap font-large  text-[14px] pl-[5px] pr-[5px] text-[#ffffff] ">Upload</p>
-      </button>
-      </div>
+          <h2 className="text-[#3F434A] text-2xl font-medium">Attachments</h2>
+          <button className="ml-[380px] flex bg-renal-blue pl-[6px] rounded-xl pr-[6px] p-[8px]">
+            <Image
+              src={getBasicIcon("upload")}
+              className={`w-[18px] svg-white`}
+              alt=""
+              width={20}
+              height={20}
+              style={{
+                objectFit: "contain",
+              }}
+            />
+            <p className="whitespace-nowrap font-large  text-[14px] pl-[5px] pr-[5px] text-[#ffffff] ">Upload</p>
+          </button>
+        </div>
         <div className="py-5 text-gray text-small">
           <p className="text-[#3F434A] font-semibold">
             Demo Product A documents for customer
           </p>
-          <div className="mt-[10px] ml-auto mr-[220px] mx-[10px] flex flex-col gap-y-2.5">
-            <div className="text-[14px] pl-[10px] py-[20px] text-[#8A9099] leading-[21px] flex justify-between bg-[#F5F5F5] rounded-xl">
-              <div className="w-[140px]">
-                <p className="text-[#3F434A]">Wireframe UI kit.pdf</p>
-                <p className="text-xs">5.6 MB</p>
+          <div className="mt-[8px] ml-auto mr-[220px] mx-[10px] flex flex-col gap-y-1.5">
+            <div className="text-[14px] pl-[10px] py-[8px] text-[#8A9099] leading-[21px] flex justify-between bg-[#F5F5F5] rounded-xl">
+              <Image
+                src={getBasicIcon("PDF")}
+                className={`w-[55px] svg-"fill-#F26868"`}
+                alt=""
+                width={20}
+                height={30}
+                style={{
+                  objectFit: "contain",
+                }}
+              />
+              <div className="w-[170px] ml-2 mt-3">
+                <p className="text-[#3F434A] ">Wireframe UI kit.pdf</p>
+                <p className="text-xs mb-2">5.6 MB</p>
               </div>
               <div className="w-[190px] flex items-center">
                 <Image
                   src={getBasicIcon("download")}
-                  className={`w-[22px] ml-[155px]`}
+                  className={`w-[22px] ml-[125px]`}
                   alt="Download"
                   width={17}
                   height={17}
@@ -46,7 +56,7 @@ const Attachements = () => {
               </div>
               <Image
                 src={getBasicIcon("delete")}
-                className={`w-[22px] mr-[20px]`}
+                className={`w-[22px] mr-[7px]`}
                 alt="delete"
                 width={17}
                 height={17}
@@ -58,20 +68,30 @@ const Attachements = () => {
             <p className="text-[#8A9099] ml-2">23 January 2023,3:00PM</p>
           </div>
         </div>
-        <div className="py-1 text-gray text-small">
+        <div className="text-gray text-small">
           <p className="text-[#3F434A] font-semibold">
             Demo Product A documents for customer
           </p>
-          <div className="mt-[10px] ml-auto mr-[220px] mx-[10px] flex flex-col gap-y-2.5">
-            <div className="text-[14px] pl-[10px] py-[20px] text-[#8A9099] leading-[21px] flex justify-between bg-[#F5F5F5] rounded-xl">
-              <div className="w-[140px]">
+          <div className="mt-[8px] ml-auto mr-[220px] mx-[10px] flex flex-col gap-y-1.5">
+            <div className="text-[14px] pl-[10px] py-[8px] text-[#8A9099] leading-[21px] flex justify-between bg-[#F5F5F5] rounded-xl">
+            <Image
+                src={getBasicIcon("pdf")}
+                className={`w-[55px] svg-grey`}
+                alt=""
+                width={20}
+                height={20}
+                style={{
+                  objectFit: "contain",
+                }}
+              />
+              <div className="w-[170px] ml-2 mt-3">
                 <p className="text-[#3F434A]">Wireframe UI kit.zip</p>
-                <p className="text-xs">5.6 MB</p>
+                <p className="text-xs mb-2">5.6 MB</p>
               </div>
               <div className="w-[190px] flex items-center">
                 <Image
                   src={getBasicIcon("download")}
-                  className={`w-[22px] ml-[155px]`}
+                  className={`w-[22px] ml-[125px]`}
                   alt="Download"
                   width={17}
                   height={17}
@@ -82,7 +102,7 @@ const Attachements = () => {
               </div>
               <Image
                 src={getBasicIcon("delete")}
-                className={`w-[22px] mr-[20px]`}
+                className={`w-[22px] mr-[7px]`}
                 alt="delete"
                 width={17}
                 height={17}
@@ -94,20 +114,30 @@ const Attachements = () => {
             <p className="text-[#8A9099] ml-2">23 January 2023,3:00PM</p>
           </div>
         </div>
-        <div className="py-2 text-gray text-small">
+        <div className="py-5 text-gray text-small">
           <p className="text-[#3F434A] font-semibold">
             Demo Product A documents for customer
           </p>
-          <div className="mt-[10px] ml-auto mr-[220px] mx-[10px] flex flex-col gap-y-2.5">
-            <div className="text-[14px] pl-[10px] py-[20px] text-[#8A9099] leading-[21px] flex justify-between bg-[#F5F5F5] rounded-xl">
-              <div className="w-[140px]">
+          <div className="mt-[8px] ml-auto mr-[220px] mx-[10px] flex flex-col gap-y-1.5">
+            <div className="text-[14px] pl-[10px] py-[8px] text-[#8A9099] leading-[21px] flex justify-between bg-[#F5F5F5] rounded-xl">
+            <Image
+                src={getBasicIcon("pdf")}
+                className={`w-[55px] svg-"fill-#F26868"`}
+                alt=""
+                width={20}
+                height={20}
+                style={{
+                  objectFit: "contain",
+                }}
+              />
+              <div className="w-[170px] ml-2 mt-3">
                 <p className="text-[#3F434A]">Wireframe UI kit.doc</p>
-                <p className="text-xs">5.6 MB</p>
+                <p className="text-xs mb-2">5.6 MB</p>
               </div>
               <div className="w-[190px] flex items-center">
                 <Image
                   src={getBasicIcon("download")}
-                  className={`w-[22px] ml-[155px]`}
+                  className={`w-[22px] ml-[125px]`}
                   alt="Download"
                   width={17}
                   height={17}
@@ -118,7 +148,7 @@ const Attachements = () => {
               </div>
               <Image
                 src={getBasicIcon("delete")}
-                className={`w-[22px] mr-[20px]`}
+                className={`w-[22px] mr-[7px]`}
                 alt="delete"
                 width={17}
                 height={17}
@@ -130,20 +160,30 @@ const Attachements = () => {
             <p className="text-[#8A9099] ml-2">23 January 2023,3:00PM</p>
           </div>
         </div>
-        <div className="py-2 text-gray text-small">
+        <div className="text-gray text-small">
           <p className="text-[#3F434A] font-semibold">
             Demo Product A documents for customer
           </p>
-          <div className="mt-[10px] ml-auto mr-[220px] mx-[10px] flex flex-col gap-y-2.5">
-            <div className="text-[14px] pl-[10px] py-[20px] text-[#8A9099] leading-[21px] flex justify-between bg-[#F5F5F5] rounded-xl">
-              <div className="w-[140px]">
+          <div className="mt-[8px] ml-auto mr-[220px] mx-[10px] flex flex-col gap-y-1.5">
+            <div className="text-[14px] pl-[10px] py-[8px] text-[#8A9099] leading-[21px] flex justify-between bg-[#F5F5F5] rounded-xl">
+            <Image
+                src={getBasicIcon("PDF")}
+                className={`w-[55px] svg-"fill-#F26868"`}
+                alt=""
+                width={20}
+                height={30}
+                style={{
+                  objectFit: "contain",
+                }}
+              />
+              <div className="w-[170px] mt-3 ml-2">
                 <p className="text-[#3F434A]">Wireframe UI kit.jpg</p>
-                <p className="text-xs">5.6 MB</p>
+                <p className="text-xs mb-3">5.6 MB</p>
               </div>
               <div className="w-[190px] flex items-center">
                 <Image
                   src={getBasicIcon("download")}
-                  className={`w-[22px] ml-[155px]`}
+                  className={`w-[22px] ml-[125px]`}
                   alt="Download"
                   width={17}
                   height={17}
@@ -154,7 +194,7 @@ const Attachements = () => {
               </div>
               <Image
                 src={getBasicIcon("delete")}
-                className={`w-[22px] mr-[20px]`}
+                className={`w-[22px] mr-[7px]`}
                 alt="delete"
                 width={17}
                 height={17}
