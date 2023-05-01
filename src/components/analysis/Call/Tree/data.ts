37e6f1527e8781e7dff811648bd6ade98e5aff16
unknown <partharora557@gmail.com>
2023-05-01 08:45:57 +0530

    done

--- src/components/analysis/Call/Tree/data.ts
+++ src/components/analysis/Call/Tree/data.ts
@@ -4,33 +4,118 @@ export const options = [
       show: false,
     },
     chart: {
-      height: 500,
-      width: 300,
+      type: "treemap",
+      toolbar: {
+        show: false,
+      },
+    },
+    colors: ["#22CCE2", "#22CCE2", "#22CCE2", "#22CCE2"],
+    plotOptions: {
+      treemap: {
+        distributed: true,
+        enableShades: false,
+      },
+    },
+  },
+  {
+    legend: {
+      show: false,
+    },
+    chart: {
+      type: "treemap",
+      toolbar: {
+        show: false,
+      },
+    },
+    colors: ["#FF965D", "#FF965D", "#FF965D", "#FF965D"],
+    plotOptions: {
+      treemap: {
+        distributed: true,
+        enableShades: false,
+      },
+    },
+  },
+  {
+    legend: {
+      show: false,
+    },
+    chart: {
+      toolbar: {
+        show: false,
+      },
       type: "treemap",
     },
     colors: [
-      "#22CCE2",
-      "#22CCE2",
-      "#22CCE2",
-      "#22CCE2",
-      "#22CCE2",
-      "#22CCE2",
-      "#22CCE2",
-      "#22CCE2",
-      "#22CCE2",
-      "#22CCE2",
-      "#22CCE2",
-      "#22CCE2",
-      "#22CCE2",
-      "#22CCE2",
-      "#22CCE2",
-      "#22CCE2",
-      "#22CCE2",
-      "#22CCE2",
-      "#22CCE2",
-      "#22CCE2",
-      "#22CCE2",
-      "#22CCE2",
+      "#FFD240",
+      "#FFD240",
+      "#FFD240",
+      "#FFD240",
+      "#FFD240",
+      "#FFD240",
+      "#FFD240",
+      "#FFD240",
+    ],
+    plotOptions: {
+      treemap: {
+        distributed: true,
+        enableShades: false,
+      },
+    },
+  },
+  {
+    legend: {
+      show: false,
+    },
+    chart: {
+      toolbar: {
+        show: false,
+      },
+      type: "treemap",
+    },
+    colors: ["#49C96D", "#49C96D", "#49C96D", "#49C96D", "#49C96D", "#49C96D"],
+    plotOptions: {
+      treemap: {
+        distributed: true,
+        enableShades: false,
+      },
+    },
+  },
+  {
+    legend: {
+      show: false,
+    },
+    chart: {
+      type: "treemap",
+      toolbar: {
+        show: false,
+      },
+    },
+    colors: ["#B656EB", "#B656EB", "#B656EB", "#B656EB", "#B656EB"],
+    plotOptions: {
+      treemap: {
+        distributed: true,
+        enableShades: false,
+      },
+    },
+  },
+  {
+    legend: {
+      show: false,
+    },
+    chart: {
+      toolbar: {
+        show: false,
+      },
+      type: "treemap",
+    },
+    colors: [
+      "#FD72DE",
+      "#FD72DE",
+      "#FD72DE",
+      "#FD72DE",
+      "#FD72DE",
+      "#FD72DE",
+      "#FD72DE",
     ],
     plotOptions: {
       treemap: {
@@ -59,6 +144,132 @@ export const data = [
         },
         {
           x: "Voice Mail",
+          y: 90,
+        },
+      ],
+    },
+  ],
+  [
+    {
+      data: [
+        {
+          x: "Real time cues",
+          y: 70,
+        },
+        {
+          x: "Post Call Analytics",
+          y: 60,
+        },
+        {
+          x: "Integration",
+          y: 70,
+        },
+      ],
+    },
+  ],
+  [
+    {
+      data: [
+        {
+          x: "Open Ended",
+          y: 70,
+        },
+        {
+          x: "Sales Coaching",
+          y: 60,
+        },
+        {
+          x: "Sales Visibility",
+          y: 70,
+        },
+      ],
+    },
+  ],
+  [
+    {
+      data: [
+        {
+          x: "Dialer",
+          y: 70,
+        },
+        {
+          x: "Revenue Potential",
+          y: 60,
+        },
+        {
+          x: "CRM",
+          y: 70,
+        },
+        {
+          x: "Web Conferencing",
+          y: 90,
+        },
+        {
+          x: "Decision Maker",
+          y: 90,
+        },
+      ],
+    },
+  ],
+  [
+    {
+      data: [
+        {
+          x: "Better Sales Visibility",
+          y: 70,
+        },
+        {
+          x: "Conversion rate",
+          y: 60,
+        },
+        {
+          x: "Improved Customer Experience",
+          y: 70,
+        },
+        {
+          x: "Ramp Time",
+          y: 90,
+        },
+        {
+          x: "Actionable Insights",
+          y: 90,
+        },
+        {
+          x: "Data Driven Coaching",
+          y: 90,
+        },
+      ],
+    },
+  ],
+  [
+    {
+      data: [
+        {
+          x: "Trial",
+          y: 70,
+        },
+        {
+          x: "Next Steps",
+          y: 60,
+        },
+        {
+          x: "Email",
+          y: 70,
+        },
+        {
+          x: "Zoom Demo",
+          y: 90,
+        },
+        {
+          x: "Meeting",
+          y: 90,
+        },
+        {
+          x: "Closure",
+          y: 90,
+        },
+        {
+          x: "Call",
           y: 90,
         },
       ],
