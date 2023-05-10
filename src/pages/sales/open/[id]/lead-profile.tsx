4c19b7e5c023aa2d03b5fe548e259a0792c7db1b
unknown <partharora557@gmail.com>
2023-05-10 12:34:37 +0530

    done

--- src/pages/sales/open/[id]/lead-profile.tsx
+++ src/pages/sales/open/[id]/lead-profile.tsx
@@ -3,12 +3,12 @@ import Navigation from "@/components/app/Navigation";
 import ProfilePage from "@/components/Profile/ProfilePage/LeadProfile";
 import LeadProfileContainer from "@/components/Profile/LeadProfileContainer";
 import dummy from "@/shared/dummy";
+import axios from "axios";
 
 //Manya will make this page
 
-const Profile = () => {
+const Profile = ({ data }: any) => {
   const titles = ["DEAL INFO", "ACTIVITY HISTORY", "ATTACHMENTS", "COACHING"];
-
   return (
     <div className="w-[100%] min-h-[90vh] pl-[40px] pr-[40px]">
       <Navigation
@@ -31,10 +31,11 @@ const Profile = () => {
         ]}
       />
       <div className="w-[100%] flex gap-[25px] mb-[100px] ">
-        <div className="w-[320px] min-h-[70vh] bg-white rounded-xl shrink-0 p-[20px]">
-          <ProfilePage />
+        <div className="w-[340px] min-h-[70vh] bg-white rounded-xl shrink-0 p-[20px]">
+          <ProfilePage data={data.result} />
         </div>
         <LeadProfileContainer
+          data={data.result}
           titles={titles}
           current={0}
           info={dummy.leadInfo}
@@ -46,3 +47,15 @@ const Profile = () => {
 };
 
 export default Profile;
+
+export async function getServerSideProps({ query, params }: any) {
+  const response = await axios.get(
+    `https://testsalescrm.nextsolutions.in/api/leads/find-by-id?id=${params.id}`
+  );
+  return {
+    props: {
+      // TODO: Can do better error handling here by passing another property error in the component
+      data: response.data || {},
+    }, // will be passed to the page component as props
+  };
+}
