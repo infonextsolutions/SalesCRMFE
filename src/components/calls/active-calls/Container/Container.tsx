f55a326894bccb0164b5d02b8e2e2e57bf130775
unknown <partharora557@gmail.com>
2023-05-10 13:18:58 +0530

    done

--- src/components/calls/active-calls/Container/Container.tsx
+++ src/components/calls/active-calls/Container/Container.tsx
@@ -1,15 +1,22 @@
 import Kanban from "@/components/View/Kanban";
 import CallsTable from "@/components/View/Tables/calls/active-calls/Calls";
 import ButtonDropDown from "@/utils/Button/Button";
-import React from "react";
+import React, { Suspense, useState } from "react";
 import Search from "../Search/Search";
+import Spinner from "@/components/loader/spinner";
 
-const CallsContainer = ({ dummy1 ,data}: LeadContainerProps) => {
-  console.log(data)
+const CallsContainer = ({ dummy1, data }: LeadContainerProps) => {
+
+  const [search, setSearch] = useState("");
+  const onChange = (e: any) => {
+    const val = e.target.value;
+    setSearch(val);
+  };
+  console.log(data);
   return (
     <div className="pr-[10px] w-[100%] bg-white min-h-[70vh] rounded-[18px] overflow-hidden mb-[40px]">
       <div className="w-[100%] h-[58px] flex items-center  px-[8px]">
-        <Search />
+        <Search   change={onChange}  />
         {/* <ButtonDropDown
           light={true}
           text={"Actions"}
@@ -19,7 +26,9 @@ const CallsContainer = ({ dummy1 ,data}: LeadContainerProps) => {
           list={[]}
         /> */}
       </div>
-      <CallsTable result={data.result} totalRecords={dummy1.totalRecords} />
+      <Suspense fallback={<Spinner />}>
+        <CallsTable totalRecords={dummy1.totalRecords} search={search} />
+      </Suspense>
     </div>
   );
 };
@@ -29,5 +38,5 @@ export default CallsContainer;
 interface LeadContainerProps {
   dummy1: any;
   dummy2: any;
-  data:any
+  data: any;
 }
