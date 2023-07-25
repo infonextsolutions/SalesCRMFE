80538b37db7d01cf84c8ff4d2d17dbed02b98862
unknown <partharora557@gmail.com>
2023-07-25 11:14:12 +0530

    done

--- src/components/Profile/AudioProfileDetails/Notes.tsx
+++ src/components/Profile/AudioProfileDetails/Notes.tsx
@@ -1,5 +1,5 @@
 import { getBasicIcon, getRoundedAvatar } from "@/utils/AssetsHelper";
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import Image from "next/image";
 import Backdrop from "@/components/View/Backdrop/Center";
 import Notesd from "./notedummy";
@@ -7,11 +7,11 @@ import axios from "axios";
 import { useAppDispatch } from "@/store/store";
 import { setError, setSuccess } from "@/store/ai";
 
-const Note = ({ title, content }: any) => {
+const Note = ({ title, content, date }: any) => {
   return (
-    <div className="mb-[30px] min-h-[250px] w-[100%] px-[28px] border-black border-[2px] py-[28px] pb-[40px] rounded-[20px] ">
+    <div className="mb-[30px] shrink-0 mt-[5px] min-h-[250px] w-[100%] px-[28px] border-black border-[2px] py-[28px] pb-[40px] rounded-[20px] ">
       <p className="font-medium border-b-[2px]  flex border-[#E8E9EB] border-dashed pb-[10px] mb-[20px] text-gray-600">
-        12 June 2020
+        {date}
       </p>
       <p className=" text-[18px] font-medium text-[#3F434A] capitalize">
         {title}
@@ -22,18 +22,10 @@ const Note = ({ title, content }: any) => {
   );
 };
 
-const Notes = ({ data }: any) => {
-  const [list, setList] = useState<any>([
-    // {
-    //   title: "note-1",
-    //   content: `  lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis euismod
-    // faucibus tempor. Pellentesque habitant morbi tristique senectus et netus
-    // et malesuada fames ac turpis egestas. Sed id nisi id turpis sagittis
-    // tincidunt...`,
-    // },
-  ]);
+const Notes = ({ data, refresh }: any) => {
+  const [list, setList] = useState<any>([...data.notes]);
 
-  console.log(data.result.notes, "please ch-11", data);
+  console.log(data.notes, "please ch-11", data);
 
   const [notes, setNotes] = React.useState(false);
   const [bool, setBool] = React.useState(true);
@@ -44,29 +36,42 @@ const Notes = ({ data }: any) => {
     title: "",
     content: "",
   });
+
+  const containerRef:any = useRef();
+
   const dispatch = useAppDispatch();
   const cancelNotes = (e: any) => {
     if (e) {
       // setList([...list, e]);
-      const url = "https://testsalescrm.nextsolutions.in/api/calling/notes";
+      const url =
+        "https://testsalescrm.nextsolutions.in/api/v2/active-call/notes";
       const { title, content } = e;
-
       console.log(e, "please ch-11");
       axios
         .post(url, {
           title: title,
           content: content,
-          callId: data._id,
+          id: data._id,
         })
         .then((e) => {
-          console.log(e,"ch-11");
+          if (containerRef.current) {
+            containerRef.current.scrollTop = 0; // Set scrollTop to 0 to scroll to the top
+          }
+          setList([
+            ...list,
+            {
+              title: title,
+              content: content,
+            },
+          ]);
           dispatch(
             setSuccess({
               show: true,
               success: "Note Added Successfully!",
             })
           );
-        }).catch((e)=>{
+        })
+        .catch((e) => {
           console.log(e);
           dispatch(
             setError({
@@ -74,7 +79,7 @@ const Notes = ({ data }: any) => {
               error: "Error Occured!",
             })
           );
-        })
+        });
     }
     setBool(false);
     setTimeout(() => {
@@ -82,6 +87,40 @@ const Notes = ({ data }: any) => {
       setBool(true);
     }, 1700);
   };
+
+  function convertISODateToString(isoDateString: any) {
+    if (isoDateString) {
+      // Create a new Date object using the ISO date string
+      const dateObject = new Date(isoDateString);
+
+      // Define the month names array
+      const monthNames = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+      ];
+
+      // Extract the day, month, and year from the date object
+      const day = dateObject.getDate();
+      const monthIndex = dateObject.getMonth();
+      const year = dateObject.getFullYear();
+
+      // Create the formatted date string
+      const formattedDate = `${day} ${monthNames[monthIndex]} ${year}`;
+
+      return formattedDate;
+    }
+    return "-"
+  }
 
   return (
     <>
@@ -126,9 +165,18 @@ const Notes = ({ data }: any) => {
           </div>
         </div>
         <div className="py-2"></div>
-        <div className=" px-[10%] overflow-y-auto h-[90vh]">
+        <div
+          ref={containerRef} className=" px-[10%] overflow-y-auto flex flex-col-reverse custom-scroll-black h-[90vh]">
           {list.map((item: any, i: any) => {
-            return <Note title={item.title} key={i} content={item.content} />;
+            console.log(item, "che21441");
+            return (
+              <Note
+                title={item.title}
+                key={i}
+                content={item.content}
+                date={convertISODateToString(item.updatedAt)}
+              />
+            );
           })}
         </div>
       </div>
