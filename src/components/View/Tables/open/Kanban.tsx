d947d07bbc1bccaf8353f33b88390604f22eb15f
unknown <partharora557@gmail.com>
2023-07-26 14:19:53 +0530

    done

--- src/components/View/Tables/open/Kanban.tsx
+++ src/components/View/Tables/open/Kanban.tsx
@@ -30,12 +30,9 @@ const KanbanTable = ({ totalRecords, search }: TableProps) => {
       const filtered = data.filter(
         (e: Lead) =>
           e.companyId?.company_name.includes(search) ||
-          e.customerId?.contact?.includes(search) ||
+          e.customerId?.name.includes(search) ||
           e.potential_deal_size?.includes(search) ||
-          e.leadStatus?.includes(search) ||
-          e.leadStage?.includes(search) ||
-          e.customerId?.email?.includes(search) ||
-          e.companyId?.company_website_url?.includes(search)
+          e?.leadId?.includes(search)
       );
 
       // const filtered = data;
