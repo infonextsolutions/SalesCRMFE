0882ff011d338f058b094d8ef4f70f01a7c380c4
ArijitPatra2906 <patraarijit440@gmail.com>
2023-11-27 11:41:17 +0530

    filter functionality implememt

--- src/components/calls/recorded-calls/Container/Container.tsx
+++ src/components/calls/recorded-calls/Container/Container.tsx
@@ -1,19 +1,49 @@
 import Kanban from "@/components/View/Kanban";
 import CallsTable from "@/components/View/Tables/calls/recorded-calls/Calls";
 import ButtonDropDown from "@/utils/Button/Button";
-import React, { Suspense, useState } from "react";
+import React, { Suspense, useEffect, useState } from "react";
 import Search from "../Search/Search";
 import Spinner from "@/components/loader/spinner";
 import DatePicker from "@/utils/Button/DatePicker";
+import axios from "axios";
 
 const CallsContainer = ({ dummy1, data }: LeadContainerProps) => {
   const [startDate, setStartDate] = useState("");
   const [endDate, setEndDate] = useState("");
   const [search, setSearch] = useState("");
+  const [companyName, setCompanyName] = useState("ABC Corp");
+  const [product, setProduct] = useState(" Product A");
+  const [callOwner, setCallOwner] = useState("John");
+  const [callType, setCallType] = useState("Product Demo");
   const onChange = (e: any) => {
     const val = e.target.value;
     setSearch(val);
   };
+
+  const getData = async () => {
+    const payload = {
+      companyName,
+      product,
+      callOwner,
+      callType,
+      search,
+      date: {
+        from: startDate,
+        to: endDate,
+      },
+    };
+
+    const response = await axios.post(
+      "https://testsalescrm.nextsolutions.in/api/leads/find-all?leadStatus=Close",
+      payload
+    );
+    dummy1 = { ...response.data };
+  };
+
+  useEffect(() => {
+    getData();
+  }, [product, companyName, callOwner, callType, startDate, endDate, search]);
+
   return (
     <div className="w-[100%] bg-white min-h-[70vh] rounded-[18px] relative mb-[40px]">
       <div className="w-[100%] h-[120px] flex items-center  px-[8px]">
@@ -30,7 +60,11 @@ const CallsContainer = ({ dummy1, data }: LeadContainerProps) => {
           <div className="flex items-center gap-5">
             <div className="flex items-center w-56 justify-between bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
               <h2 className="font-medium">Company Name</h2>
-              <select className="text-red-500" id="countries">
+              <select
+                onChange={(e) => setCompanyName(e.target.value)}
+                className="text-red-500"
+                id="countries"
+              >
                 <option selected value="Open">
                   ABC Corp
                 </option>
@@ -38,7 +72,11 @@ const CallsContainer = ({ dummy1, data }: LeadContainerProps) => {
             </div>
             <div className="flex items-center w-52 justify-between bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
               <h2 className="font-medium">Product/Service</h2>
-              <select className="text-red-500" id="countries">
+              <select
+                onChange={(e) => setProduct(e.target.value)}
+                className="text-red-500"
+                id="countries"
+              >
                 <option selected value="Open">
                   Product A
                 </option>
@@ -47,7 +85,11 @@ const CallsContainer = ({ dummy1, data }: LeadContainerProps) => {
             </div>
             <div className="flex items-center w-36 justify-between bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
               <h2 className="font-medium">Call Owner</h2>
-              <select className="text-red-500" id="countries">
+              <select
+                onChange={(e) => setCallOwner(e.target.value)}
+                className="text-red-500"
+                id="countries"
+              >
                 <option selected value="Open">
                   John
                 </option>
@@ -55,7 +97,11 @@ const CallsContainer = ({ dummy1, data }: LeadContainerProps) => {
             </div>
             <div className="flex items-center w-52 justify-between bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
               <h2 className="font-medium">Call Type</h2>
-              <select className="text-red-500" id="countries">
+              <select
+                onChange={(e) => setCallType(e.target.value)}
+                className="text-red-500"
+                id="countries"
+              >
                 <option selected value="Open">
                   Product Demo
                 </option>
--- src/components/leads/open/Container/index.tsx
+++ src/components/leads/open/Container/index.tsx
@@ -1,13 +1,14 @@
 // import LeadsTable from "@/components/View/Tables/LeadsSearch";
 import Lead from "@/types/Leads";
 import ButtonDropDown from "@/utils/Button/Button";
-import React, { useState, Suspense } from "react";
+import React, { useState, Suspense, useEffect } from "react";
 import Search from "../../genUtils/Search";
 // import KanbanContainer from "@/components/View/Kanban";
 import { useSelector } from "react-redux";
 import Spinner from "@/components/loader/spinner";
 import KanbanTable from "@/components/View/Tables/open/Kanban";
 import DatePicker from "@/utils/Button/DatePicker";
+import axios from "axios";
 
 const LeadsTable = React.lazy(
   () => import("@/components/View/Tables/open/LeadsSearch")
@@ -19,12 +20,39 @@ const LeadsContainer = ({ view, records, list }: LeadContainerProps) => {
   const [startDate, setStartDate] = useState("");
   const [endDate, setEndDate] = useState("");
   const [search, setSearch] = useState("");
+  const [status, setStatus] = useState("Open");
+  const [stage, setStage] = useState("Enquiry");
+  const [product, setProduct] = useState(" Product A");
+  const [leadSource, setLeadSource] = useState("Website");
   const onChange = (e: any) => {
     const val = e.target.value;
     setSearch(val);
   };
-
   const state = useSelector((state: any) => state.auth);
+
+  const getData = async () => {
+    const payload = {
+      status,
+      stage,
+      product,
+      leadSource,
+      search,
+      date: {
+        from: startDate,
+        to: endDate,
+      },
+    };
+
+    const response = await axios.post(
+      "https://testsalescrm.nextsolutions.in/api/leads/find-all?leadStatus=Open",
+      payload
+    );
+    records = { ...response.data };
+  };
+
+  useEffect(() => {
+    getData();
+  }, [status, stage, product, leadSource, startDate, endDate, search]);
 
   return (
     <div className="w-[100%] bg-white min-h-[70vh] rounded-[18px] relative mb-[40px]">
@@ -42,7 +70,11 @@ const LeadsContainer = ({ view, records, list }: LeadContainerProps) => {
           <div className="flex items-center gap-5">
             <div className="flex items-center w-36 justify-between bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
               <h2 className="font-medium">Status</h2>
-              <select className="text-red-500" id="countries">
+              <select
+                onChange={(e) => setStatus(e.target.value)}
+                className="text-red-500"
+                id="countries"
+              >
                 <option selected value="Open">
                   Open
                 </option>
@@ -51,7 +83,11 @@ const LeadsContainer = ({ view, records, list }: LeadContainerProps) => {
             </div>
             <div className="flex items-center w-36 justify-between bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
               <h2 className="font-medium">Stage</h2>
-              <select className="text-red-500" id="countries">
+              <select
+                onChange={(e) => setStage(e.target.value)}
+                className="text-red-500"
+                id="countries"
+              >
                 <option selected value="Open">
                   Enquiry
                 </option>
@@ -59,7 +95,11 @@ const LeadsContainer = ({ view, records, list }: LeadContainerProps) => {
             </div>
             <div className="flex items-center w-52 justify-between bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
               <h2 className="font-medium">Product/Service</h2>
-              <select className="text-red-500" id="countries">
+              <select
+                onChange={(e) => setProduct(e.target.value)}
+                className="text-red-500"
+                id="countries"
+              >
                 <option selected value="Open">
                   Product A
                 </option>
@@ -68,7 +108,11 @@ const LeadsContainer = ({ view, records, list }: LeadContainerProps) => {
             </div>
             <div className="flex items-center w-52 justify-between bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
               <h2 className="font-medium">Lead Source</h2>
-              <select className="text-red-500" id="countries">
+              <select
+                onChange={(e) => setLeadSource(e.target.value)}
+                className="text-red-500"
+                id="countries"
+              >
                 <option selected value="Open">
                   Website
                 </option>
