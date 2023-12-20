cf44474c66fd6364ae524e1fec1a817dc869c86c
tanish-th <tanish@techhelps.co.in>
2023-12-20 13:37:15 +0530

    changes

--- src/views/teams/TalkingPoint/subTabs/OTPD.tsx
+++ src/views/teams/TalkingPoint/subTabs/OTPD.tsx
@@ -1,8 +1,188 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Table from '../../Table';
+import Pagination from '../../Pagination';
+import Filters from '../../Filters';
 
 const OTPD = () => {
+    const columns = [
+        {
+            width: 200,
+            left: 40,
+            text: "Talking Point",
+        },
+        {
+            width: 120,
+            left: 20,
+            text: "Total Duration",
+        },
+        {
+            width: 120,
+            left: 20,
+            text: "Average Duration",
+        },
+    ];
+
+    const columns2 = [
+        {
+            width: 200,
+            left: 40,
+            text: "Call Title",
+        },
+        {
+            width: 120,
+            left: 20,
+            text: "Call Duration",
+        },
+        {
+            width: 200,
+            left: 20,
+            text: "Call Date & Time",
+        },
+        {
+            width: 200,
+            left: 20,
+            text: "Call Owner",
+        },
+        {
+            width: 200,
+            left: 20,
+            text: "Call Participants",
+        },
+    ];
+
+    const [rows, setRows] = useState([
+        [
+            {
+                text: "Introduction",
+                // subText: "Sales Manager",
+            },
+            {
+                text: "55 min",
+            },
+            {
+                text: "35 min",
+            },
+        ],
+        [
+            {
+                text: "New Leads",
+                // subText: "Sales Manager",
+            },
+            {
+                text: "55 min",
+            },
+            {
+                text: "35 min",
+            },
+        ],
+        [
+            {
+                text: "Sound Deal",
+                // subText: "Sales Manager",
+            },
+            {
+                text: "55 min",
+            },
+            {
+                text: "35 min",
+            },
+        ],
+    ]);
+
+    const [rows2, setRows2] = useState([
+        [
+            {
+                text: "Introduction",
+            },
+            {
+                text: "55 min",
+            },
+            {
+                text: "23 January 2023 3:00 pm",
+            },
+            {
+                text: "John C.",
+                subText: "Sales Manager",
+            },
+            {
+                text: "Shraddha P.",
+                subText: "Aarti"
+            },
+        ]
+    ]);
+
+    const [filters, setFilters] = useState({
+        callTitle: {
+            label: "Call Title",
+            options: [
+                { key: "Product Discussion", label: "Product Discussion" },
+            ],
+            value: "",
+        },
+        callDate: {
+            label: "Call Date",
+            value: "",
+            type: "DATE",
+        },
+        companyName: {
+            label: "Company Name",
+            options: [
+                { key: "ABC Corp.", label: "ABC Corp." },
+            ],
+            value: "",
+        },
+        callOwner: {
+            label: "Call Owner",
+            options: [
+                { key: "John C.", label: "John C." },
+            ],
+            value: "",
+        },
+        productService: {
+            label: "Product/Service",
+            options: [
+                { key: "Product A", label: "Product A" },
+                { key: "Product B", label: "Product B" },
+                { key: "Product C", label: "Product C" },
+                { key: "Product D", label: "Product D" },
+            ],
+            value: "",
+        },
+        callType: {
+            label: "Call Type",
+            options: [
+                { key: "Product Demo", label: "Product Demo" },
+            ],
+            value: "",
+        },
+        callDisposition: {
+            label: "Call Disposition",
+            options: [
+                { key: "Follow-Up required", label: "Follow-Up required" },
+            ],
+            value: "",
+        },
+    });
+
+    const handleFilterUpdate = (filterData: any, newValue: any) => {
+        console.log('======= UPDATE FILTER : filterData, newValue =======', filterData, newValue);
+    };
+
     return (
-        <div>OTPD</div>
+        <div>
+            <Filters filters={filters} onUpdate={handleFilterUpdate} />
+            <div className='w-[100%] flex'>
+                <div className=''>
+                    <Table rows={rows} columns={columns} />
+                    <Pagination />
+                </div>
+                <div className=''>
+                    <h1 className='text-black text-[22px]'>List of Calls</h1>
+                    <Table columns={columns2} rows={rows2} />
+                    <Pagination />
+                </div>
+            </div>
+        </div>
     )
 }
 
--- src/views/teams/TalkingPoint/subTabs/PTPD.tsx
+++ src/views/teams/TalkingPoint/subTabs/PTPD.tsx
@@ -1,8 +1,132 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Table from '../../Table'
+import Pagination from '../../Pagination';
+import Filters from '../../Filters';
 
 const PTPD = () => {
+    const columns = [
+        {
+            width: 200,
+            left: 40,
+            text: "Talking Members",
+        },
+        {
+            width: 120,
+            left: 20,
+            text: "Total Time",
+        },
+        {
+            width: 120,
+            left: 20,
+            text: "Average Duration",
+        },
+    ];
+
+    const [rows, setRows] = useState([
+        [
+            {
+                text: "John",
+                // subText: "Sales Manager",
+            },
+            {
+                text: "55 sec",
+            },
+            {
+                text: "35 sec",
+            },
+        ],
+        [
+            {
+                text: "Xavier",
+                // subText: "Sales Manager",
+            },
+            {
+                text: "55 sec",
+            },
+            {
+                text: "35 sec",
+            },
+        ],
+        [
+            {
+                text: "Mitest",
+                // subText: "Sales Manager",
+            },
+            {
+                text: "55 sec",
+            },
+            {
+                text: "35 sec",
+            },
+        ],
+    ]);
+
+    const [filters, setFilters] = useState({
+        callDate: {
+            label: "Call Date",
+            value: "",
+            type: "DATE",
+        },
+        companyName: {
+            label: "Company Name",
+            options: [
+                { key: "ABC Corp.", label: "ABC Corp." },
+            ],
+            value: "",
+        },
+        callOwner: {
+            label: "Call Owner",
+            options: [
+                { key: "John C.", label: "John C." },
+            ],
+            value: "",
+        },
+        productService: {
+            label: "Product/Service",
+            options: [
+                { key: "Product A", label: "Product A" },
+                { key: "Product B", label: "Product B" },
+                { key: "Product C", label: "Product C" },
+                { key: "Product D", label: "Product D" },
+            ],
+            value: "",
+        },
+        callType: {
+            label: "Call Type",
+            options: [
+                { key: "Product Demo", label: "Product Demo" },
+            ],
+            value: "",
+        },
+        callDisposition: {
+            label: "Call Disposition",
+            options: [
+                { key: "Follow-Up required", label: "Follow-Up required" },
+            ],
+            value: "",
+        },
+        callTitle: {
+            label: "Call Title",
+            options: [
+                { key: "Product Discussion", label: "Product Discussion" },
+            ],
+            value: "",
+        },
+        selectTalkingPoint: {
+            label: "Select Talking Point",
+            options: [
+                { key: "New Leads", label: "New Leads" },
+            ],
+            value: "",
+        },
+    });
+
     return (
-        <div>PTPD</div>
+        <div>
+            <Filters filters={filters} />
+            <Table columns={columns} rows={rows} />
+            <Pagination />
+        </div>
     )
 }
 
