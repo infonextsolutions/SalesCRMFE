ef812456692556e38c97aea4a351953d50d1c115
ArijitPatra2906 <patraarijit440@gmail.com>
2023-12-21 18:14:52 +0530

    Auth page Footer

---
+++ src/components/utils/AuthFooter.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const AuthFooter = () => {
+  return (
+    <div>
+      <div className="bg-[#2E3134] px-28 h-[66px] flex flex-wrap items-center justify-between">
+        <div className="flex flex-wrap gap-6">
+          <p className="text-white font-normal">Privacy Policy</p>
+          <p className="text-white font-normal">Cookie Policy</p>
+        </div>
+        <div className="flex flex-wrap gap-6">
+          <p className="text-white font-normal">Help & Support</p>
+          <p className="text-white font-normal">©365 Sales 2023</p>
+        </div>
+      </div>
+      ;
+    </div>
+  );
+};
+
+export default AuthFooter;
